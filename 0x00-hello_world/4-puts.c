610349b5fa0dccd18887d3a1e9cab24cf6c7909e
Briyad37 <byleander37@gmail.com>
2024-02-21 08:40:02 -0800

    accomodarted the file with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,11 @@
 /**
  * a file for using puts to purinti
  */
-int main(void) {
-	const char *alx="Programmming is like bulding a multilingual puzzle.";
+int main(void)
+{
+	const char *alx = "Programming is like bulding a multilingual puzzle";
+	
 	puts(alx);
 
-	return 0;
+	return (0);
 }
