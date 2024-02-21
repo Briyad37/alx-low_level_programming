351b6b523dffc3bd95161b1b492feb26d7695b78
Briyad37 <byleander37@gmail.com>
2024-02-21 11:13:43 -0800

    simple change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
  * main -prints "Programming is like bulding a multilingual puzzle"
- * Return -always 0
+ *
+ * Return: 0 on success, non-zero on failure
  */
 int main(void)
 {
-	const char *alx = "\"Programming is like bulding a multilingual puzzle";
+	const char *alx = "\"Programming is like building a multilingual puzzle";
 
 	puts(alx);
 	return (0);
