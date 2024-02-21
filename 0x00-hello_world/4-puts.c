c38b3435a21d6a1bb1caeb301e2b51923a1093c7
Briyad37 <byleander37@gmail.com>
2024-02-21 08:29:15 -0800

    4 puts added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * a file for using puts to purinti
+ */
+int main(void) {
+	const char *alx="Programmming is like bulding a multilingual puzzle.";
+	puts(alx);
+
+	return 0;
+}
