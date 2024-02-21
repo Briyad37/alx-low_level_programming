f62ea5d039728aa9dc9b7f3bce0c329df485288a
Briyad37 <byleander37@gmail.com>
2024-02-21 11:28:06 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main -prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * returns: 0 when suceesful, non-zero with failure
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
