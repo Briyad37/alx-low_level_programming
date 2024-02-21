2838ea327fe0a3f22a3205155cac4eb98fc93a4a
Briyad37 <byleander37@gmail.com>
2024-02-21 09:03:44 -0800

    betty coreection

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-
 /**
- * a file for using puts to purinti
+ * main -prints "Programming is like bulding a multilingual puzzle
+ * Return -always 0
  */
 int main(void)
 {
 	const char *alx = "Programming is like bulding a multilingual puzzle";
-	
-	puts(alx);
 
+	puts(alx);
 	return (0);
 }
