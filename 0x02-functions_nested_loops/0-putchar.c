b3a35596f45f95a7fc0eedd3751a868e49db57c4
Briyad37 <byleander37@gmail.com>
2024-03-06 21:49:54 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,16 @@
  * return : always 0
  */
 
-int main (void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-        _putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return(0);
+\t_putchar('_');
+\t_putchar('p');
+\t_putchar('u');
+\t_putchar('t');
+\t_putchar('c');
+\t_putchar('h');
+\t_putchar('a');
+\t_putchar('r');
+\t_putchar('\n');
+\treturn (0);
 }
