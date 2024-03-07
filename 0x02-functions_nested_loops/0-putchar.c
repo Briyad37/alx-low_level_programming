a72e7bc7f13c080424b86e6accc87b6798c7523c
Briyad37 <byleander37@gmail.com>
2024-03-06 22:18:23 -0800

    yep2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,15 @@
 
 /**
  * main - entry point
- * return : always 0
+ * return : always 0 (succes)
  */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
