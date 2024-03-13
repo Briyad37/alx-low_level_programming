57c87f5f1264ab07664df62e4e838f2349cd696e
Briyad37 <byleander37@gmail.com>
2024-03-13 08:46:32 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main -prints sizes of variable types
  *
- * Returns: 0 on  sucees, non-zero on failures
+ * Return: 0 (Sucesss)
  */
 
 int main(void)
 {
-printf("Size of char: %lu bytes\n", sizeof(char));
-printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of long: %lu bytes\n", sizeof(long));
-printf("Size of long int: %lu bytes\n", sizeof(long int));
-printf("Size oflong long int : %lu bytes\n", sizeof(long long int));
-printf("size of float : %lu bytes\n", sizeof(float));
+printf("Size of char: %lu bytes(s)\n", sizeof(char));
+printf("Size of int: %lu bytes(s)\n", sizeof(int));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+printf("Size oflong long int: %lu byte(s)\n", sizeof(long long int));
+printf("size of float: %lu byte(s)\n", sizeof(float));
 
 return (0);
 
